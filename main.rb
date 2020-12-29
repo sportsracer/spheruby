2f244fe5ecd9e22fbaa7eae5d538518fe58f91bc
Steffen Wenz <steffen@workpath.com>
2020-12-29 11:45:31 +0100

    Initial commit: Open an empty window

---
+++ main.rb
@@ -0,0 +1,27 @@
+require "optparse"
+require "gosu"
+
+options = {
+  :width => 800,
+  :height => 600,
+}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{$0} [-w/--width WIDTH] [-h/--height HEIGHT]"
+
+  opts.on("-w WIDTH", "--width WIDTH", Integer) do |width|
+    options[:width] = width
+  end
+
+  opts.on("-h HEIGHT", "--height HEIGHT", Integer) do |height|
+    options[:height] = height
+  end
+
+  opts.on("--help") do
+    puts opts
+    exit
+  end
+end.parse!
+
+w = Gosu::Window.new(options[:width], options[:height])
+w.show
